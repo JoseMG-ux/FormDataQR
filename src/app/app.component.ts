1cbbd119cd180076351be050c72e0e27ebcda374
JoseMG-ux <joseutp14@gmail.com>
2021-04-30 11:07:19 -0500

    cambios x2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -82,7 +82,7 @@ export class AppComponent implements OnInit {
           if (videoDevices.length > 0){
               let choosenDev;
               for (const dev of videoDevices){
-                  if (dev.label.includes('')){
+                  if (dev.label.includes('back')){
                       choosenDev = dev;
                       break;
                   }
